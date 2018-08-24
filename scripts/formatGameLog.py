b56c892e2ff6f8320386e9d84931dbe84266eb8c
Han Xiao <han@hanxbox.com>
2018-08-23 22:05:33 -0400

    amending to readData + error catching

--- scripts/formatGameLog.py
+++ scripts/formatGameLog.py
@@ -5,9 +5,13 @@ import pandas as pd
 
 def readData(fileName):
     """
-    Trying to read in raw data
+    Trying to read in raw data of game log formats it into a list separated by commas
     :param fileName:
     :return:
     """
     fs = open(fileName, "r")
-    return fs.read().split()
+    string_stream = fs.read()
+    list_of_values = string_stream.split()
+    if 0 == len(list_of_values):
+        raise Exception("List of values parsed incorrectly by readData() function")
+
